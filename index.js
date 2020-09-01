b41a37fadef82caa13b1c48963df21ef07cbad83
Max Upravitelev <max@upravitelev.de>
2020-09-01 18:49:10 +0200

    add jest

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// const http = require('http')
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
 
 const mongoose = require('mongoose')
 
-//
-const app = require('./app') // the actual Express application
+const app = require('./app') 
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -16,19 +11,3 @@ const server = http.createServer(app)
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
-//
-
-
-
-// const mongoUrl = config.MONGODB_URI
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// app.use(cors())
-// app.use(express.json())
-
-
-
-// const PORT = 3003
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
