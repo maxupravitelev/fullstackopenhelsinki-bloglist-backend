dbf136ff9ac1514780933a1c468196729adef0b2
Max Upravitelev <max@upravitelev.de>
2020-09-01 13:13:07 +0200

    fix post reqs

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-require('dotenv').config()
+// const http = require('http')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
 
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+//
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
+//
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-// const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(cors())
-app.use(express.json())
+// const mongoUrl = config.MONGODB_URI
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.use(cors())
+// app.use(express.json())
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
