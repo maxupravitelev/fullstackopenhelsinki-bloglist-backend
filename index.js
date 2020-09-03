84fca919b2809fa8851a285df28445fdd2086cbf
Max Upravitelev <max@upravitelev.de>
2020-09-03 11:32:51 +0200

    add totalLikes testing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing#testing-node-applications
 
 const mongoose = require('mongoose')
 
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,31 @@
 const dummy = (blogs) => {
-    return 1
+  return 1;
+};
+
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const totalLikes = (blogpostList) => {
+  let totalLikesCount = 0;
+
+  for (let i = 0; i < blogpostList.length; i++) {
+    totalLikesCount += blogpostList[i].likes;
   }
-  
-  module.exports = {
-    dummy
-  }
+
+  return totalLikesCount;
+};
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
