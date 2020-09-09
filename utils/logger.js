d884b1f1043fdbe02984b2df5aee325fc5bab116
Max Upravitelev <max@upravitelev.de>
2020-09-09 14:44:19 +0200

    disable error logger while testing

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,16 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
-    console.log(...params)
-  }  }
-  
-  const error = (...params) => {
-    console.error(...params)
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
   }
-  
-  module.exports = {
-    info, error
+};
+
+const error = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
   }
+};
+
+module.exports = {
+  info,
+  error,
+};
