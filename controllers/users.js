ff1f686fb03813949751e40ccf2573c07183573f
Max Upravitelev <max@upravitelev.de>
2020-11-16 12:16:16 +0100

    get blogs by user via user id

--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,46 @@
-const bcrypt = require("bcrypt");
-const usersRouter = require("express").Router();
-const User = require("../models/user");
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
 
-usersRouter.post("/", async (request, response) => {
-  const body = request.body;
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
 
   if (body.password.length < 3 || body.password.username < 3) {
     return response
       .status(400)
-      .json({ error: "username and password must be at least 3 chars long " });
+      .json({ error: 'username and password must be at least 3 chars long ' })
   }
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
   const user = new User({
     username: body.username,
     name: body.name,
     passwordHash,
-  });
+  })
 
-  const savedUser = await user.save();
+  const savedUser = await user.save()
 
-  response.json(savedUser);
-});
+  response.json(savedUser)
+})
 
-usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("blogs", {
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
     title: 1,
     author: 1,
     url: 1,
-  });
-  response.json(users);
-});
+  })
+  response.json(users)
+})
 
-module.exports = usersRouter;
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+  })
+  response.json(user)
+})
+
+module.exports = usersRouter
